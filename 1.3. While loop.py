9868e681b5e1eb1104b4fe87366335c404bb6780
Joyal James <75773527+joyaljms98@users.noreply.github.com>
2024-09-05 07:42:03 +0530

    Update 1.3. While loop.py

--- 1.3. While loop.py
+++ 1.3. While loop.py
@@ -1,14 +1,34 @@
 
 # 3.a) Python program using while loop to Print 'Hello World' 1000 times
 
-
-
-
+'''i=1
+while (i<=1000):
+    print("Hello world")
+    i+=1
+'''
 
 # 3.b) Python program using while loop to Find sum of first 'n' natural numbers
 
+'''
+i=1
+sum=0
+n= int(input("Enter the limit : "))
+while (i<=n):
+    sum += i
+    i+=1
+print("Sum = ", sum)
+'''
 
+# 3.c) Python program using while loop to Generate multiplication table (till 10) of any given number
 
-
-
-# 3.c) Python program using while loop to Generate multiplication table(till 10) of any given number
+'''
+i=1
+n= int(input("Enter the limit : "))
+while (i<=n):
+    j=1
+    while (j<=10):
+        print(i,"x",j,"=",i*j)
+        j+=1
+    print()
+    i+=1
+'''
