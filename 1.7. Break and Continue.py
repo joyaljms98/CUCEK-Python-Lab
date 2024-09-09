024dedfbbe1eca3d0dc527e8a242e856ce9fcf96
Joyal James <75773527+joyaljms98@users.noreply.github.com>
2024-09-09 07:54:14 +0530

    Update 1.7. Break and Continue.py

--- 1.7. Break and Continue.py
+++ 1.7. Break and Continue.py
@@ -1,8 +1,23 @@
 
 # 7.a) Python program to print First half numbers of a given limit using 'break'
 
+'''
 
+n = int(input("Enter a limit: "))
+mid = n // 2
+for i in range(1, n + 1):
+    if i > mid:
+        break
+    print(i)
+'''
 
+# 7.b) Python program to print Even numbers upto a limit of 'n' using ' continue
 
-
-# 7.b) Python program to print Even numbers upto a limit of 'n' using ' continue1
+'''
+n = int(input("Enter a limit: "))
+for i in range(1, n + 1):
+    if i%2 == 0:
+        print(i)
+    else:
+        continue
+'''
